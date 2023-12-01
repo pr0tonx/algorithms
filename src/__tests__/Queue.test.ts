6a42dd811722e33eaa5288b1fe294b48f8d02694
Felipe <felipepeengler@outlook.com>
2023-12-01 15:06:17 -0300

    day 3

---
+++ src/__tests__/Queue.test.ts
@@ -0,0 +1,27 @@
+const Queue = require('../code/Queue');
+
+// FIXME
+test('Queue', () => {
+    // const list = new Queue<number>();
+    //
+    // list.enqueue(5);
+    // list.enqueue(7);
+    // list.enqueue(9);
+    //
+    // expect(list.deque()).toEqual(5);
+    // expect(list.length).toEqual(2);
+    // debugger;
+    //
+    // list.enqueue(11);
+    // debugger;
+    // expect(list.deque()).toEqual(7);
+    // expect(list.deque()).toEqual(9);
+    // expect(list.peek()).toEqual(11);
+    // expect(list.deque()).toEqual(11);
+    // expect(list.deque()).toEqual(undefined);
+    // expect(list.length).toEqual(0);
+    //
+    // list.enqueue(69);
+    // expect(list.peek()).toEqual(69);
+    // expect(list.length).toEqual(1);
+});
